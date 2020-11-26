30513acae14d0977d1924d2dc36a6bdfada8eb10
martynas3113 <martynas3113@users.noreply.github.com>
2020-11-26 17:55:53 +0200

    Render=clock

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,7 @@
 import { renderSocials } from "./socials/components/renderSocials.js";
 import { socialsData } from './data/socialsData.js';
+import { renderClock } from "./socials/components/clock/renderClock.js";
 
 renderSocials('footer > .row', socialsData);
+
+renderClock('.clock');

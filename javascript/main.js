6f3046a094da2b2275f188a6dadd32084d881125
martynas3113 <martynas3113@users.noreply.github.com>
2020-11-26 15:07:13 +0200

    Documentation

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,4 @@
 import { renderSocials } from "./socials/components/renderSocials.js";
 import { socialsData } from './data/socialsData.js';
 
-renderSocials(socialsData);
-
-
+renderSocials('footer > .row', socialsData);

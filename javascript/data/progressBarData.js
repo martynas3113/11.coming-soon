4cfabe6538ac98ebce1fbd5046637b1983aa1b79
martynas3113 <martynas3113@users.noreply.github.com>
2020-11-30 20:26:55 +0200

    Update progressBarData.js

--- javascript/data/progressBarData.js
+++ javascript/data/progressBarData.js
@@ -2,17 +2,17 @@ const progressBarData = [
     {
         selector: '.column.left',
         title: 'Web development',
-        value: '90'
+        value: 90
     },
     {
         selector: '.column.left',
         title: 'Web design',
-        value: '80'
+        value: 80
     },
     {
         selector: '.column.left',
         title: 'UX',
-        value: '40'
+        value: 40
     },
     
 ];
